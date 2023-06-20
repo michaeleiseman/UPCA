505bb04c0995bc151e97b34bcd498c2a3c5d6d02
michaeleiseman <eiseman@att.net>
2023-06-20 15:30:13 -0400

    initial commit

---
+++ Day05Exercises/Day05_Exercise07_HighlightMidtoneShadow/HighlightMidtoneShadow/HighlightMidtoneShadow.js
@@ -0,0 +1,29 @@
+let colorBox = document.getElementById("colorBox");
+let button = document.getElementById("pickAColor");
+button.addEventListener("click", generateColor, false);
+function generateColor() {
+    /* Obtain random numbers for red, green, and blue. Note that
+    you can generate a random number between 0 and 255 inclusive
+    by using the formula Math.floor(256 * Math.random()) */
+    
+
+
+    /* Now change the "background-color" property of the colorBox's
+    style attribute to "rgb(" + r + ", " + g + ", " + b + ")" where r, g, and b are
+    each a variable whose value is a number between 0 and 255 inclusive*/
+    
+
+    //make a variable named "sum" that is the sum of red, green and blue values.
+    
+    //if sum is greater than 510
+    
+        //label the color as a highlight color in the output span
+        
+    //if sum is greater than 255
+   
+        //label the color as a midtone color in the output span
+    
+        
+    //otherwize label the color as a shadow color in the output span.
+    
+}
